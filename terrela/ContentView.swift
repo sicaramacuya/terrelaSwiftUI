2d2b5b46978eb2a5ea2913b8139bc5c98ad0b91d
sicaramacuya <eric.morales-rodriguez@students.makeschool.com>
2021-03-17 13:34:14 -0400

    Initial Commit

---
+++ terrela/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  terrela
+//
+//  Created by Eric Morales on 3/17/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ terrela/terrelaApp.swift
@@ -0,0 +1,17 @@
+//
+//  terrelaApp.swift
+//  terrela
+//
+//  Created by Eric Morales on 3/17/21.
+//
+
+import SwiftUI
+
+@main
+struct terrelaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
